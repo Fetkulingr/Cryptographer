25006e8c8573dba96e4254827612a40a921e3a1c
Grigory Fetkulin <103204349+Fetkulingr@users.noreply.github.com>
2023-12-17 17:03:38 +0300

    Refactored code to comply with flake8 standards

--- Cryptographer/main.py
+++ Cryptographer/main.py
@@ -1,52 +1,81 @@
+'''
+This module make
+
+Athor: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
+Starting 2022/08/20
+Ending 2024//
+
+'''
+import os
 from tkinter import *
+from tkinter import Button, Label, Menu
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
+from typing import List
 import pyAesCrypt
-import os
-# Функция кнопки "Зашифровать"
-def crypt(filemass):
+
+# The function of the "Encrypt" button
+
+
+def crypt(filemass: List[str]):
+    """AI is creating summary for crypt
+
+    Args:
+        filemass (List[str]): The list of selected files to encrypt
+    """
     password = passField.get()
-    bufferSize = 512*1024
-    pyAesCrypt.encryptFile(str(filemass[0]), str(filemass[0]) + '.aes', password, bufferSize)
+    buffer_size = 512*1024
+    pyAesCrypt.encryptFile(str(filemass[0]), str(
+        filemass[0]) + '.aes', password, buffer_size)
     filemass.clear()
 
 
-# Функция кнопки "Расшифровать"
-def decrypt(filemass):
+# The function of the "Decrypt" button
+def decrypt(filemass: List[str]):
+    """AI is creating summary for decrypt
+
+    Args:
+        filemass (List[str]): The list of selected files to decrypt
+    """
     password = passField.get()
-    bufferSize = 512*1024
-    pyAesCrypt.decryptFile(str(filemass[0]), str(os.path.splitext(filemass[0])[0]), password, bufferSize)
+    buffer_size = 512*1024
+    pyAesCrypt.decryptFile(str(filemass[0]), str(
+        os.path.splitext(filemass[0])[0]), password, buffer_size)
     filemass.clear()
 
 
 def file_dialog():
+    """AI is creating summary for file_dialog
+    """
     file_name = fd.askopenfilename()
     filemass.append(file_name)
 
 
-filemass = []
-# Интерфейс программы
+filemass: List[str] = []
+# Program interface
 root = Tk()
 root.title("Криптограф")
 root.geometry("300x250")
 
 
 def show_info():
+    """AI is creating summary for show_info
+    """
     mb.showinfo("О программе", "Феткулин Григорий - Криптограф, 2022")
 
 
-label1 = Label( text='Введите пароль:')
-label1.place(x = 2, y = 50)
+label1 = Label(text='Введите пароль:')
+label1.place(x=2, y=50)
 
 passField = Entry(show='*', width='37')
-passField.place(x = 2, y = 80)
+passField.place(x=2, y=80)
 
 
 btn = Button(text='Расшифровать',  command=lambda: decrypt(filemass))
-btn.place(x = 210, y = 210)
+btn.place(x=210, y=210)
 
 btn = Button(text='Зашифровать', command=lambda: crypt(filemass))
-btn.place(x = 7, y = 210)
+btn.place(x=7, y=210)
 
 main_menu = Menu()
 file_menu = Menu()
